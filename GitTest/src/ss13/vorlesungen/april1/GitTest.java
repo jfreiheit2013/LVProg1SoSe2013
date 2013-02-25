610136563f5b036377ce70568760af67c0c02938
Jörn Freiheit <freiheit@htw-berlin.de>
2013-02-25 16:39:40 +0100

    die Ausgabe wurde geändert

--- GitTest/src/ss13/vorlesungen/april1/GitTest.java
+++ GitTest/src/ss13/vorlesungen/april1/GitTest.java
@@ -4,7 +4,7 @@ public class GitTest {
 
 
 	public static void main(String[] args) {
-		System.out.println("Mein erstes Projekt in Git");
+		System.out.println("Jetzt habe ich etwas geändert");
 
 	}
 
